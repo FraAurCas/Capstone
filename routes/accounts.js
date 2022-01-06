700db41908d9de2cab9b3fb8da53d1a25f6d7045
FraAurCas <frankcasella17@gmail.com>
2022-01-06 15:20:35 -0500

    Added data and route directs.
    data has one dummy data value and accounts file in /routes has a start

---
+++ routes/accounts.js
@@ -0,0 +1,16 @@
+var express = require('expresss');
+var router = express.Router();
+module.exports = router;    
+
+var events_data = require('../data/dummy_accounts');
+
+// GET the main page wip from Anton
+/*router.get('/', function(req, res, next){
+    
+});
+*/
+
+// GET the detailed pages
+router.get('/:account_id', function(req,res,next){
+    let account_id = req.params.account_id;
+});
