eab5659a021bd17a86ab825d2542a6a82df9c74d
FraAurCas <frankcasella17@gmail.com>
2022-01-14 15:17:21 -0500

    Accounts and account_details have a good start

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRouter = require('./routes/main');
+var accountsRouter = require('./routes/accounts');
 
 var app = express();
 
@@ -26,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/main', mainRouter)
+app.use('/main', mainRouter);
+app.use('/accounts', accountsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/accounts.js
+++ routes/accounts.js
@@ -1,8 +1,8 @@
-var express = require('expresss');
+var express = require('express');
 var router = express.Router();
 module.exports = router;    
 
-var events_data = require('../../../data/dummy_accounts');
+var accounts_data = require('../data/dummy_accounts');
 
 // GET the main page wip from Anton
 /*router.get('/', function(req, res, next){
@@ -13,4 +13,12 @@ var events_data = require('../../../data/dummy_accounts');
 // GET the detailed pages
 router.get('/:account_id', function(req,res,next){
     let account_id = req.params.account_id;
+    let account = accounts_data.find(function (evt) {return evt.account_id == account_id});
+
+    if (account === undefined) {
+        next();
+    }
+    else{
+        res.render('account_detail', { title: account.account_name, styles: ["tables", "account"], account: account});
+    }
 });
