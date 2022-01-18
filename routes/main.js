29819c825e65c0b584e62ab6873820bcd02e1821
Particleboy04 <particleboy04@gmail.com>
2022-01-18 15:22:21 -0500

    I hate merge conflicts

--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
-module.exports = router;
 
-<<<<<<< HEAD
 var mysql = require('mysql');
 var account_data;
-=======
-var account_data = require('../data/dummy_accounts');
->>>>>>> 2071871d7c269ff845b08ca79401a441c1ff8b80
+
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -26,13 +22,9 @@ con.connect(function (err) {
 
 /* GET main page. */
 router.get('/', function (req, res, next) {
-<<<<<<< HEAD
     res.render('main', { title: 'Policies', array: account_data });
 });
 
 
 module.exports = router;
-=======
-    res.render('main', { title: 'Policies', array: mask(account_data) });
-}); 
->>>>>>> 2071871d7c269ff845b08ca79401a441c1ff8b80
+
