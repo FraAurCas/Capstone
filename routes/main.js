b4cbc91ad0603e1abdb9c1b0cb34ff39d5d87968
Particleboy04 <particleboy04@gmail.com>
2022-01-21 15:18:47 -0500

    Got the first column excluder thing done

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('mysql');
-var account_data;
-
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -22,7 +20,20 @@ con.connect(function (err) {
 
 /* GET main page. */
 router.get('/', function (req, res, next) {
-    res.render('main', { title: 'Policies', array: account_data });
+    var include_office = (req.query.include_office || 'false') === 'true';
+
+    var query_string = "SELECT name, "
+    if (!include_office) { //Why does this need to be negated?
+        query_string += "office, ";
+    }
+    query_string += "broker, clientAdvocate, industry, revenue, footprint, limits, losses, retentions FROM test_accounts";
+    
+    con.query(query_string, function (err, result, fields) {
+        if (err) throw err;
+        account_data = result;
+    });
+    
+    res.render('main', { title: 'Policies', array: account_data, include_office: include_office });
 });
 
 
