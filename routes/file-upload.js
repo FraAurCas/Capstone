e205f10c4d96b01eb1896ca1deb3ffe4e27dc1f8
Particleboy04 <particleboy04@gmail.com>
2022-03-21 15:06:11 -0400

    Used Formidable

--- routes/file-upload.js
+++ routes/file-upload.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var con = require('./database');
 var XLSX = require("xlsx");
+var formidable = require('formidable');
 
 router.get('/', function(req, res, next) {
   res.render('file-upload', { title: 'File Upload' });
@@ -9,12 +10,20 @@ router.get('/', function(req, res, next) {
     
 router.post('/', function(req, res, next) {
 
-      const entry_file = document.getElementById('entry_file').value || "";
-      
-      var workbook = XLSX.readFile(entry_file);
+  new formidable.IncomingForm().parse(req, (err, fields, files) => {
+    if (err) {
+      console.error('Error', err)
+      throw err
+    }
+    console.log('Fields', fields)
+    //console.log('Files', files)
+    for (const file of Object.entries(files)) {
+      var workbook = XLSX.readFile(file[1].filepath);
       var sheet_name_list = workbook.SheetNames;
       var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
       console.log(xlData);
+    }
+  })
     
   res.render('file-upload', { title: 'File Upload' });
 });
