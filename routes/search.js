6d6ee75c19dc5effcc0691ca076e6877e65a024c
Particleboy04 <particleboy04@gmail.com>
2022-03-22 15:18:44 -0400

    I hate webdev

--- routes/search.js
+++ routes/search.js
@@ -1,26 +1,25 @@
-    var express = require('express');
-    var expressHbs = require('express-handlebars');
-    const { handlebars } = require('hbs');
+var express = require('express');
+var expressHbs = require('express-handlebars');
+const { handlebars } = require('hbs');
 
-    var router = express.Router();
+var router = express.Router();
 
-    var hbs = expressHbs.create({
-        helpers: require('../helpers/handlebars').helpers,
-        defaultLayout: 'layout',
-        extname: '.hbs'
-    });
+var hbs = expressHbs.create({
+    helpers: require('../helpers/handlebars').helpers,
+    defaultLayout: 'layout',
+    extname: '.hbs'
+});
 
-    // const hbsHelpers = require('../helpers/handlebars');
+// const hbsHelpers = require('../helpers/handlebars');
 
-    console.log('===============')
-    console.log(hbs.handlebars.helpers);
-    var con = require('./database');
-
-
-    router.get('/', function(req,res, next) {
-    res.render('search', {title: 'Search'});
-    });
+console.log('===============')
+console.log(hbs.handlebars.helpers);
+var con = require('./database');
 
 
-    
-    module.exports = router;
+router.get('/', function(req,res, next) {
+    res.render('search', {title: 'Search', IDVals: ["One", "Two", "Three"]});
+}); 
+
+
+module.exports = router;
